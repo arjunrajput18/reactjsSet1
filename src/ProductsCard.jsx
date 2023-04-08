5c1b9a33adabbf758cc387c481e6cb45597ce8cf
Arjunsingh Rajput <31860250+arjunrajput18@users.noreply.github.com>
2023-04-08 15:09:28 +0530

    new way

--- src/ProductsCard.jsx
+++ src/ProductsCard.jsx
@@ -7,3 +7,24 @@ export const ProductsCard = ({ products }) => {
     </>
   );
 };
+
+
+-----------
+
+
+export const ProductData=()=>{
+  const products = [
+    { name: 'Perk', quantity: 10, sales: 7 },
+    { name: 'Pepsi', quantity: 10, sales: 20 },
+    { name: 'Coke', quantity: 18, sales: 50 },
+    { name: 'Maggi', quantity: 41, sales: 22 },
+    { name: '5Star', quantity: 7, sales: 9 },
+  ]
+
+ 
+ const sortedData=[...products].sort((a,b)=>a.sales-b.sales)
+ console.log(sortedData)
+ return (<ul>
+{products.map(({name},i)=><li key={i}>{name}</li>)}
+  </ul>)
+}
